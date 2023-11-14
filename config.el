d3fcfd2a4fd5642607a0d47852d743271ca69733
zielOS <ahsanur041@gmail.com>
2023-11-13 22:12:42 -0700

    first commit

---
+++ config.el
@@ -0,0 +1,205 @@
+(defmacro bookmark-selector-launcher (NAME WIDTH HEIGHT FUNCTION)
+  "Define a launcher command.
+
+  Bookmark-selector is a package revolving around using emacs
+  outside of emacs to browse your bookmarks. Most of the commands
+  defined, consist of opening an emacs frame with only a
+  minibuffer, with a specified NAME, WIDTH and HEIGHT and inside it
+  calling FUNCTION and deleting the frame after the function
+  completes or is canceled."
+  `(with-selected-frame (make-frame '((name . ,NAME)
+                                      (minibuffer . only)
+                                      (width . ,WIDTH)
+                                      (height . ,HEIGHT)))
+     (unwind-protect
+         (funcall ,FUNCTION)
+       (delete-frame))))
+
+(defun emacs-run-launcher ()
+  "Create and select a frame called emacs-run-launcher which consists only of a minibuffer and has specific dimensions. Runs app-launcher-run-app on that frame, which is an emacs command that prompts you to select an app and open it in a dmenu like behaviour. Delete the frame after that command has exited"
+  (interactive)
+  (bookmark-selector-launcher "emacs-run-launcher" 59 20 'app-launcher-run-app)
+  (unwind-protect
+      (funcall ,FUNCTION)
+    (delete-frame)))
+
+(setq centaur-tabs-set-bar 'over
+      centaur-tabs-set-icons t
+      centaur-tabs-gray-out-icons 'buffer
+      centaur-tabs-height 24
+      centaur-tabs-set-modified-marker t
+      centaur-tabs-style "bar"
+      centaur-tabs-modified-marker "•")
+(map! :leader
+      :desc "Toggle tabs globally" "t c" #'centaur-tabs-mode
+      :desc "Toggle tabs local display" "t C" #'centaur-tabs-local-mode)
+(evil-define-key 'normal centaur-tabs-mode-map (kbd "g <right>") 'centaur-tabs-forward        ; default Doom binding is 'g t'
+                                               (kbd "g <left>")  'centaur-tabs-backward       ; default Doom binding is 'g T'
+                                               (kbd "g <down>")  'centaur-tabs-forward-group
+                                               (kbd "g <up>")    'centaur-tabs-backward-group)
+
+(setq fancy-splash-image "~/.config/doom/doom-logo.png")
+
+(map! :leader
+      (:prefix ("d" . "dired")
+       :desc "Open dired" "d" #'dired
+       :desc "Dired jump to current" "j" #'dired-jump)
+      (:after dired
+       (:map dired-mode-map
+        :desc "Peep-dired image previews" "d p" #'peep-dired
+        :desc "Dired view file"           "d v" #'dired-view-file)))
+
+(evil-define-key 'normal dired-mode-map
+  (kbd "M-RET") 'dired-display-file
+  (kbd "h") 'dired-up-directory
+  (kbd "l") 'dired-open-file ; use dired-find-file instead of dired-open.
+  (kbd "m") 'dired-mark
+  (kbd "t") 'dired-toggle-marks
+  (kbd "u") 'dired-unmark
+  (kbd "C") 'dired-do-copy
+  (kbd "D") 'dired-do-delete
+  (kbd "J") 'dired-goto-file
+  (kbd "M") 'dired-do-chmod
+  (kbd "O") 'dired-do-chown
+  (kbd "P") 'dired-do-print
+  (kbd "R") 'dired-do-rename
+  (kbd "T") 'dired-do-touch
+  (kbd "Y") 'dired-copy-filenamecopy-filename-as-kill ; copies filename to kill ring.
+  (kbd "Z") 'dired-do-compress
+  (kbd "+") 'dired-create-directory
+  (kbd "-") 'dired-do-kill-lines
+  (kbd "% l") 'dired-downcase
+  (kbd "% m") 'dired-mark-files-regexp
+  (kbd "% u") 'dired-upcase
+  (kbd "* %") 'dired-mark-files-regexp
+  (kbd "* .") 'dired-mark-extension
+  (kbd "* /") 'dired-mark-directories
+  (kbd "; d") 'epa-dired-do-decrypt
+  (kbd "; e") 'epa-dired-do-encrypt)
+;; Get file icons in dired
+(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+;; With dired-open plugin, you can launch external programs for certain extensions
+;; For example, I set all .png files to open in 'sxiv' and all .mp4 files to open in 'mpv'
+(setq dired-open-extensions '(("gif" . "sxiv")
+                              ("jpg" . "sxiv")
+                              ("png" . "sxiv")
+                              ("mkv" . "mpv")
+                              ("mp4" . "mpv")))
+
+(evil-define-key 'normal peep-dired-mode-map
+  (kbd "j") 'peep-dired-next-file
+  (kbd "k") 'peep-dired-prev-file)
+(add-hook 'peep-dired-hook 'evil-normalize-keymaps)
+
+(setq delete-by-moving-to-trash t
+      trash-directory "~/.local/share/Trash/files/")
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(setq doom-theme 'catppuccin
+      doom-themes-treemacs-enable-variable-pitch nil)
+(map! :leader
+      :desc "Load new theme" "h t" #'counsel-load-theme)
+
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 27)
+      doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font" :size 27)
+      doom-big-font (font-spec :family "JetBrainsMono Nerd Font" :size 32))
+(after! doom-themes
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t))
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic)
+  '(font-lock-keyword-face :slant italic))
+
+(remove-hook! '(org-mode-hook text-mode-hook conf-mode-hook)
+	      #'display-line-numbers-mode)
+
+(use-package! doom-modeline
+  :config
+  (setq doom-modeline-persp-name t
+        doom-modeline-height 35))
+
+(use-package! org
+  :config
+  (setq org-highlight-links
+        '(bracket angle plain tag date footnote))
+  ;; Setup custom links
+  (+org-init-custom-links-h))
+
+(use-package! org-modern
+  :hook (org-mode . org-modern-mode)
+  :config
+  (setq
+   ;; Edit settings
+   org-catch-invisible-edits 'show-and-error
+   org-special-ctrl-a/e t
+   org-insert-heading-respect-content t
+   ;; Appearance
+   org-modern-radio-target    '("❰" t "❱")
+   org-modern-internal-target '("↪ " t "")
+   org-modern-todo nil
+   org-modern-tag nil
+   org-modern-timestamp t
+   org-modern-statistics nil
+   org-modern-progress nil
+   org-modern-priority nil
+   org-modern-horizontal-rule "──────────"
+   org-modern-hide-stars "·"
+   ;;org-modern-star ["⁖"]
+   org-modern-keyword "‣"
+   org-modern-list '((43 . "•")
+                     (45 . "–")
+                     (42 . "↪")))
+  (custom-set-faces!
+    `((org-modern-tag)
+      :background ,(doom-blend (doom-color 'blue) (doom-color 'bg) 0.1)
+      :foreground ,(doom-color 'grey))
+    `((org-modern-radio-target org-modern-internal-target)
+      :inherit 'default :foreground ,(doom-color 'blue)))
+  )
+
+(setq org-src-window-setup 'current-window)
+
+(use-package! ob-python
+  :commands org-babel-execute:python)
+
+(setq lsp-pylsp-plugins-flake8-max-line-length 88)
+
+(use-package! python-black
+  :demand t
+  :after python
+  :config
+  (add-hook! 'python-mode-hook #'python-black-on-save-mode)
+  (map! :leader :desc "Blacken Buffer" "m b b" #'python-black-buffer)
+  (map! :leader :desc "Blacken Region" "m b r" #'python-black-region)
+  (map! :leader :desc "Blacken Statement" "m b s" #'python-black-statement))
---
+++ packages.el
@@ -0,0 +1,69 @@
+;; theme-packages
+(package! catppuccin-theme)
+
+;; tabs-packages
+(package! centaur-tabs)
+
+;; company-packages
+(package! company-box)
+(package! company-dict)
+(package! company-mode)
+
+;; vertico-packages
+(package! all-the-icons-completion)
+(package! consult)
+(package! embark)
+(package! embark-consult)
+(package! marginalia)
+(package! orderless)
+(package! vertico)
+(package! wgrep)
+
+;; treemacs-packages
+(package! treemacs)
+(package! treemacs-projectile)
+(package! treemacs-evil)
+(package! treemacs-magit)
+(package! lsp-treemacs)
+
+;; dired-packages
+(package! all-the-icons-dired)
+(package! diff-hl)
+(package! diredfl)
+(package! dired-rsync)
+(package! fd-dired)
+
+;; org-packages
+(package! htmlize)
+(package! ob-async)
+(package! org-cliplink)
+(package! orgit)
+(package! org)
+(package! org-contrib)
+(package! ox-clip)
+(package! evil-org)
+(package! org-pdftools)
+(package! org-download)
+(package! gnuplot)
+(package! gnuplot-mode)
+(package! ob-ipython)
+(package! jupyter)
+(package! org-roam)
+(package! org-noter)
+(package! org-modern)
+(package! org-appear)
+(package! all-the-icons)
+(package! org-super-agenda)
+
+;; python-packages
+(package! anaconda-mode)
+(package! conda)
+(package! pip-requirements)
+(package! poetry)
+(package! cython-mode)
+(package! flycheck-cython)
+(package! lsp-pyright)
+(package! python-black)
+
+;; application-packages
+(package! app-launcher :recipe (:host github :repo "SebastienWae/app-launcher"))
