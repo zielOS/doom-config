645b8be023c6b74c79e9866010e5ab0f2b86dbb3
zielOS <ahsanur041@gmail.com>
2024-02-15 16:50:00 -0700

    updated

--- init.el
+++ init.el
@@ -169,6 +169,10 @@
         +noter
         +roam2
         +pretty)
+       (python
+        +cython
+        +lsp
+        +pyright)
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
